d4217529b91726cce187e70e7630754b2f7e37ec
Naven Kennedy <r2h@AdminisatorsMBP.attlocal.net>
2020-03-22 18:50:26 -0400

    changes committed

--- Week6-Javascript/ChallengesEasy/ChallengesEasy.js
+++ Week6-Javascript/ChallengesEasy/ChallengesEasy.js
@@ -1,3 +1,4 @@
 var Name1 = 'Thomas';
 var Name2 = 'John';
-console.log('"The name ' + Name1 + ' is longer than ' + Name2 + ' by 2 characters" ') ;
+characters = Name1.length - Name2.length;
+console.log('"The name ' + Name1 + ' is longer than ' + Name2 + ' by ' + characters + ' characters"') ;
