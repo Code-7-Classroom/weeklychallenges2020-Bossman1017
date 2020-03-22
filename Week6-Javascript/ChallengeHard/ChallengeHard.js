21c5876cc914b77c52a8d8f633714e8d85c089f8
Naven Kennedy <r2h@AdminisatorsMBP.attlocal.net>
2020-03-22 07:55:10 -0400

    changes commit

--- Week6-Javascript/ChallengeHard/ChallengeHard.js
+++ Week6-Javascript/ChallengeHard/ChallengeHard.js
@@ -5,16 +5,15 @@ function add(num1, num2){
     return num1 + num2;
 }
 
-x=add(5, 7);
 
 //Subtract
-var x = subtract(5, 7);
+var x = subtract(13, 3);
 
 function subtract(num1, num2){
     return num1 - num2;
 }
 
-x=subtract(13, 3);
+
 
 //Multiply
 var x = multiply(5, 7);
@@ -23,7 +22,7 @@ function multiply(num1, num2){
     return num1 *num2;
 }
 
-x=multiply(13, 3);
+
 
 //Divide
 var x = divide(5, 7);
@@ -32,4 +31,3 @@ function divide(num1, num2){
     return num1 - num2;
 }
 
-x=divide(100, 25);
