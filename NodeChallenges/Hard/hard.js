e38a41853048831749babec282f3d9d9caa08142
Naven Kennedy <r2h@AdminisatorsMBP.attlocal.net>
2020-04-29 01:05:01 -0400

    changes added

--- NodeChallenges/Hard/hard.js
+++ NodeChallenges/Hard/hard.js
@@ -7,9 +7,7 @@ let data=require(`./employee.json`);
 const port=process.env.PORT || 5000;
 
 
-app.listen(port,()=>{
-    console.log(`Server running on port ${port}`)
-})
+app.listen(port,() => console.log(`Server running on port ${port}`));
 
 app.get(`/employees`, (req,res)=>{
     if (!data){
